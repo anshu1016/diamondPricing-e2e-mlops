a4aae86a97ae525019b938fdd816db563743319d
anshu1016 <arunshukla98710@gmail.com>
2025-05-07 11:52:09 +0530

    Preprocessing done

--- setup.py
+++ setup.py
@@ -1,43 +1,75 @@
+# from setuptools import setup, find_packages
+
+# __version__ = "0.0.4"
+# REPO_NAME = "MLOPS_Project"
+# PKG_NAME = "PyDB_Connector"
+# AUTHOR_USER_NAME = "anshu1016"
+# AUTHOR_EMAIL = "arunshukla98710@gmail.com"
+
+# with open('README.md', 'r', encoding='utf-8') as f:
+#     long_description = f.read()
+
+# setup(
+#     name=PKG_NAME,
+#     version=__version__,
+#     author=AUTHOR_USER_NAME,
+#     author_email=AUTHOR_EMAIL,
+#     description="A Python package for connecting with databases.",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",  # FIXED
+#     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+#     project_urls={
+#         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+#     },
+#     package_dir={"": "src"},
+#     packages=find_packages(where="src"),
+#     install_requires=[
+#         "pymongo",
+#         "dnspython",
+#         "pandas",
+#         "numpy",
+#         "ensure",
+#     ],  # ONLY runtime deps, no `-e .` or test tools here
+# )
+
+
 from setuptools import setup, find_packages
-from typing import List
 
-HYPEN_E_DOT='-e .'
+__version__ = "1.0.1"
+REPO_NAME = "MLOPS_Project"
+PKG_NAME = "PyDB_Connector"
+AUTHOR_USER_NAME = "anshu1016"
+AUTHOR_EMAIL = "arunshukla98710@gmail.com"
 
-'''def get_requiremet(file_path:str)->List[str]:
-    requirements = []
-    with open(file_path) as f:
-        requirements=f.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
-        
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements'''
-
-   
 with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()     
-   
-
-__version__ = "0.0.4"
-REPO_NAME = "mongodb_connector"
-PKG_NAME= "MongoDB-Connect"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+    long_description = f.read()
 
 setup(
     name=PKG_NAME,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A python package for connecting with database.",
+    description="A Python package for connecting with databases.",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    
-    
+    install_requires=[
+        "pymongo",
+        "dnspython",
+        "pandas",
+        "numpy",
+        "ensure",
+    ],
+    python_requires=">=3.7",  # added this for specifying Python version requirement
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
 )
